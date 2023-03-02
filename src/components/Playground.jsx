eb11a0d489981b61d121e0250b6c84d6bd4f9176
gnzlplcs <palaciosgm@outlook.com>
2023-03-02 11:55:34 -0500

    :bricks: create Playground component

---
+++ src/components/Playground.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Playground.css";
+
+const Playground = ({ children }) => {
+  return <div className="play-ground">{children}</div>;
+};
+
+export default Playground;
