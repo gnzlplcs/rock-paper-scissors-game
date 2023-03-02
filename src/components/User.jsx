27c43a490f9f11ea94d06a700f1df06abe3401bd
gnzlplcs <palaciosgm@outlook.com>
2023-03-02 12:48:39 -0500

    :bricks: create User component

---
+++ src/components/User.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { settings } from "../configs/game";
+import "./User.css";
+
+const User = ({ userScore, userSelection, trophyIcon, children }) => {
+  return (
+    <div className="user-card">
+      <h1>{settings.userName}</h1>
+      {userScore < settings.winTarget ? (
+        <>
+          <div className="choice-grid">{children}</div>
+          <h3>
+            {userSelection === ""
+              ? "Pick one!"
+              : `Your choice: ${userSelection}`}
+          </h3>
+        </>
+      ) : (
+        <>
+          <img src={trophyIcon} alt={Trophy} />
+          <h3>Victory!</h3>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default User;
