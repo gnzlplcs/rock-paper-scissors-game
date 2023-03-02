9191b889dae91f2d1783b75dd14a340215a0cfc3
gnzlplcs <palaciosgm@outlook.com>
2023-03-02 13:35:44 -0500

    :construction: implement the structure
    modify App component / export all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
-import "./App.css";
+import Title from "./components/Title";
+import Round from "./components/Round";
+import Playground from "./components/Playground";
+import Profile from "./components/Profile";
+import User from "./components/User";
+import Choice from "./components/Choice";
+import Computer from "./components/Computer";
+import Score from "./components/Score";
+import Message from "./components/Message";
+import Reset from "./components/Reset";
 import { settings } from "./configs/game";
 import rock from "./assets/rock.png";
 import paper from "./assets/paper.png";
 import scissors from "./assets/scissors.png";
 import trophy from "./assets/trophy.png";
+import "./App.css";
 
 const App = () => {
   const [game, setGame] = useState({
@@ -16,7 +26,49 @@ const App = () => {
     message: "",
   });
 
-  return <div className="app">Rock Paper Scissors Game</div>;
+  return (
+    <div className="app">
+      <Title />
+      <Round {...game} />
+      <Playground>
+        <Profile>
+          <User {...game} trophyIcon={trophy}>
+            <Choice
+              {...game}
+              value="Rock"
+              onClick={play}
+              choiceIcon={rock}
+            />
+            <Choice
+              {...game}
+              value="Paper"
+              onClick={play}
+              choiceIcon={paper}
+            />
+            <Choice
+              {...game}
+              value="Scissors"
+              onClick={play}
+              choiceIcon={scissors}
+            />
+          </User>
+          <Score score={userScore}/>
+        </Profile>
+        <Message {...game} />
+        <Profile>
+          <Computer
+            {...game}
+            rockIcon={rock}
+            paperIcon={paper}
+            scissorsIcon={scissors}
+            trophyIcon={trophy}
+          />
+          <Score score={pcScore} />
+        </Profile>
+      </Playground>
+      <Reset {...game} onClick={reset}/>
+    </div>
+  );
 };
 
 export default App;
